c899680122849912eb85d71da6902e1dd5e2e8be
gmfreire22 <gmfreire22@gmail.com>
2017-11-24 12:47:19 -0200

    primeiro commit

--- app/src/main/java/com/example/guilh/bandcreator/edit_profile.java
+++ app/src/main/java/com/example/guilh/bandcreator/edit_profile.java
@@ -1,13 +1,48 @@
 package com.example.guilh.bandcreator;
 
+import android.content.Intent;
+import android.graphics.Color;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
+import android.view.View;
+import android.widget.ImageView;
 
 public class edit_profile extends AppCompatActivity{
+    ImageView man_select, woman_select, done;
+    Intent intent;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.edit_profile);
+        man_select = (ImageView) findViewById(R.id.man_button);
+        woman_select = (ImageView) findViewById(R.id.woman_button);
+        done = (ImageView) findViewById(R.id.done);
+        intent = new Intent(this, search.class);
+        man_select.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                man_select.setColorFilter(0xFF0000FF);
+                woman_select.setColorFilter(0);
+                Log.i("Clicked", "Man was selected");
+            }
+        });
+        woman_select.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View v){
+                man_select.setColorFilter(0);
+                woman_select.setColorFilter(Color.rgb(255, 0,255));
+                Log.i("Clicked", "Woman was selected");
+
+            }
+        });
+        done.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View v){
+                startActivity(intent);
+            }
+        });
     }
     @Override
     public void onBackPressed() {
--- app/src/main/java/com/example/guilh/bandcreator/search.java
+++ app/src/main/java/com/example/guilh/bandcreator/search.java
@@ -4,25 +4,40 @@ import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.AdapterView;
+import android.widget.ImageView;
+import android.widget.ListView;
 import android.widget.Spinner;
 
 public class search extends AppCompatActivity implements AdapterView.OnItemSelectedListener{
     String[] instruments={"piano", "guitarra"};
+    ImageView go_to_search_button;
+    ListView listView;
     int symbols[] = {R.drawable.classic_music, R.drawable.guitar};
-    String[] styles = {"classic", "rock", "jazz", "blues", "punk"};
-    int styles_icons[] = {R.drawable.classic_music, R.drawable.classic_music, R.drawable.classic_music,R.drawable.classic_music, R.drawable.classic_music};
+    String[] styles = {"no preference", "classic", "rock", "jazz", "blues", "punk", "hip hop"};
+    int styles_icons[] = {R.drawable.black_dot, R.drawable.classic_music, R.drawable.rock, R.drawable.jazz, R.drawable.blues,R.drawable.punk, R.drawable.hip_hop};
+
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.search_for);
-
+        listView = (ListView) findViewById(R.id.listview);
          Spinner spin = (Spinner) findViewById(R.id.simpleSpinner);
             spin.setOnItemSelectedListener(this);
         CustomAdapter customAdapter=new CustomAdapter(getApplicationContext(),symbols,instruments);
         spin.setAdapter(customAdapter);
 
             Spinner spin2 = (Spinner) findViewById(R.id.simpleSpinner2);
-            CustomAdapter customAdapter2=new CustomAdapter(getApplicationContext(),styles_icons, styles);
+            CustomAdapter2 customAdapter2=new CustomAdapter2(getApplicationContext(),styles, styles_icons);
             spin2.setAdapter(customAdapter2);
+            go_to_search_button = (ImageView) findViewById(R.id.search_view);
+
+            go_to_search_button.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    //Search for here
+                    //Add results to listview
+                    //listView.setVisibility(View.VISIBLE);
+                }
+                });
         }
 
 
@@ -33,6 +48,7 @@ public class search extends AppCompatActivity implements AdapterView.OnItemSelec
 
         @Override
         public void onNothingSelected(AdapterView<?> arg0) {
-            // TODO Auto-generated method stub
+
         }
+
     }
